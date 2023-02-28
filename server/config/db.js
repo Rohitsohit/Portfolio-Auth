03d5ce83e209a7855048e320c2ca9117ca855294
Rohitsohit <itsrohitemail@gmail.com>
2023-02-27 22:13:42 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+//  file name : login.ejs
+//    Name : Rohit
+//    Student ID : 301315173
+// 	Date : 25-02-2023 
+
+
+
+
+
+
+module.exports =
+{
+    "URI":"mongodb+srv://Rohit:123@cluster0.xqoa6nc.mongodb.net/?retryWrites=true&w=majority"
+}
